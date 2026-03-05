3bf73464190ff385c59cc1548bf782e86523dd93
vishal-jaware <vishaljaware10@gmail.com>
2026-03-05 19:33:39 +0530

    cors updated

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ dotenv.config()
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    // origin:"http://localhost:5173",
+    origin:"vishal-imdb.netlify.app",
     credentials:true
 }))
 app.use(express.json())
